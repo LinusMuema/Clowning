b47b4962b7aaa65df62fb62811b91718e82e4df6
linusmuema <linus.m.muema@gmail.com>
2022-03-08 15:55:01 +0300

    - added missing internet permission lol

--- presentation/src/main/java/com/moose/clowning/MainActivity.kt
+++ presentation/src/main/java/com/moose/clowning/MainActivity.kt
@@ -23,6 +23,10 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val id = (1..10).random()
+        viewmodel.getSinglePost(id)
+
         setContent {
             ClowningTheme {
                 Surface(color = MaterialTheme.colors.background) {
