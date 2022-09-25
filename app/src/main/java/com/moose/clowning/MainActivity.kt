c3db68b4a2ef72dad2209492f604f581d690d581
linusmuema <linus.m.muema@gmail.com>
2022-09-25 15:26:13 +0200

    - you can start here

--- app/src/main/java/com/moose/clowning/MainActivity.kt
+++ app/src/main/java/com/moose/clowning/MainActivity.kt
@@ -3,9 +3,6 @@ package com.moose.clowning
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import com.moose.clowning.ui.theme.ClowningTheme
 
 
