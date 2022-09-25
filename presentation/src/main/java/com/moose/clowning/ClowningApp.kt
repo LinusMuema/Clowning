8660f2051084819f110ec22bec1adb6784736c7d
linusmuema <linus.m.muema@gmail.com>
2022-09-25 15:22:28 +0200

    - setup the basics

--- presentation/src/main/java/com/moose/clowning/ClowningApp.kt
+++
@@ -1,9 +0,0 @@
-package com.moose.clowning
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class ClowningApp: Application() {
-
-}
--- presentation/src/main/java/com/moose/clowning/ui/theme/Color.kt
+++ presentation/src/main/java/com/moose/clowning/ui/theme/Color.kt
@@ -1,8 +1,16 @@
 package com.moose.clowning.ui.theme
 
+import androidx.compose.material.lightColors
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val red = Color(0xffE74C3C)
+val grey = Color(0xFF808080)
+val white = Color(0xFFE5E5E5)
+val black = Color(0xFF000000)
+
+val ClowningColors = lightColors(
+    error = red,
+    primary = white,
+    onPrimary = black,
+    onSurface = grey,
+)
